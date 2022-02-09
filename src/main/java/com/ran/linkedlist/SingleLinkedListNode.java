e2b7e9ea2fc47e0d39a17f73edef0c8494ff9985
randhirkr <cusatrandhir@gmail.com>
2022-02-09 10:31:12 -0800

    Single Linkedlist node definition

---
+++ src/main/java/com/ran/linkedlist/SingleLinkedListNode.java
@@ -0,0 +1,14 @@
+package com.ran.linkedlist;
+
+// Definition of single LinkedList class
+public class SingleLinkedListNode {
+	
+	int value;
+	
+	SingleLinkedListNode next;
+	
+	SingleLinkedListNode(int x){
+		value = x;
+	}
+
+}
